343981d150b4be0c7d62b0a837b9c2ea3569b2bb
Adarsh <adarshmohan42@gmail.com>
2025-01-27 10:54:15 +0530

    first commit

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.2"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("net.researchgate.release") version 3.1.0'
+	id("net.researchgate.release") version "3.1.0"
 }
 
 group = "com.example"
@@ -22,6 +22,32 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+
+release {
+	failOnCommitNeeded = true
+	failOnPublishNeeded = true
+	failOnSnapshotDependencies = true
+	failOnUnversionedFiles = true
+	failOnUpdateNeeded = true
+	revertOnFail = true
+	preCommitText = ""
+	preTagCommitMessage = "[Gradle Release Plugin] - pre tag commit:"
+	tagCommitMessage = "[Gradle Release Plugin] - creating tag: "
+	newVersionCommitMessage = "[Gradle Release Plugin] - new version commit: "
+	tagTemplate = "${version}"
+	versionPropertyFile = "gradle.properties"
+	snapshotSuffix = "-SNAPSHOT"
+	pushReleaseVersionBranch = null
+
+	git {
+		requireBranch.set("master")
+		pushToRemote.set("origin")
+		pushToBranchPrefix.set("")
+		commitVersionFileOnly.set(false)
+		signTag.set(false)
+	}
 }
 
 
